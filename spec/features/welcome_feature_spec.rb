716b55615826568d5fc835cd5d0f2c4fb966f3ed
Ben Manning <bmanning@me.com>
2015-01-21 21:55:47 -0800

    wrote passing tests

--- spec/features/welcome_feature_spec.rb
+++ spec/features/welcome_feature_spec.rb
@@ -3,9 +3,14 @@ require 'rails_helper'
 feature 'Navigating Information', type: :feature do
   scenario 'Visiting the Welcome Page' do
     visit "/"
+    expect(page).to have_content('Hello World')
 
-    click_link 'See About'
-
-    expect(page).to have_content 'Hello World'
+    click_link('See About')
   end	
+
+
+  scenario 'Visiting See About Page' do
+  	visit "/welcome/about"
+    expect(find('ul')).to have_selector('li', count: 4)
+  end
 end
